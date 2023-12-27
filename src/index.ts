e4c64170fc1d3c663503dfa499701a7a9411fc15
Ramzil <ibragimovramzil53@gmail.com>
2023-12-28 00:02:47 +0500

    chore

--- src/index.ts
+++ src/index.ts
@@ -9,14 +9,27 @@ const mysqlConfig = {
   password: "secret",
   database: "test_db",
 }
+function hasProperty(obj:Object,props:String[]){
+  for(const prop of props){
+    if(!Object.prototype.hasOwnProperty.call(obj,prop)){
+      return false;
+    }
+  }
+  return true;
+}
 
 const app = new Hono()
 app.get('/', (c) => c.text('Hello Hono!'))
 
 app.get('/users', async (c) => {
     const connection = await mysql.createConnection(mysqlConfig);
-         const [rows] = await connection.execute(`SELECT * FROM users`);
-    return await c.json(rows)
+    const query=c.req.query()
+    if(hasProperty(query,["id"])){
+      const [rows] = await connection.execute(`SELECT * FROM users where id=${query["id"]}`);
+      connection.end();
+      return await c.json({response:200,message:"OK",user:rows[0]},200)
+    }
+    return c.json({response: 400,message:"Не правильный запрос"}, 400)
 })
 
 serve(app)
